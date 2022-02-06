c2448f2353e2a8bfab95e810e552865237835239
Leonard Chin <leonardchin2017@gmail.com>
2022-02-06 21:55:05 +0800

    ✨ feat: loading screen
    implement loading screen to provide better UX

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+// Loading Screen ========================================
+$(document).ready(function() {
+     // Users can skip the loading process if they want.
+     $('.skip').click(function() {
+         
+         $('.overlay, body').addClass('loaded');
+     })
+     
+     // Will wait for everything on the page to load.
+     $(window).bind('load', function() {
+         $('.overlay, body').addClass('loaded');
+         setTimeout(function() {
+             $('.overlay').css({'display':'none'})
+            
+         }, 2000)
+     });
+     
+     // Will remove overlay after 1min for users cannnot load properly.
+     setTimeout(function() {
+         $('.overlay, body').addClass('loaded');
+     }, 60000);
+ })
+
 function setIframeSrc(newSrc) {
     document.getElementById("MainPageContainer").src=newSrc;
 }
@@ -26,3 +49,4 @@ toggleIcon.addEventListener('click', () => {
         toggleIcon.className = 'menuIcon';
     }
 });
+
