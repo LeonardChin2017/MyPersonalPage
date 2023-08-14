64d98e31bdf754952662c3e44b4a848cfb929032
Leonard Chin <leonardchin2017@gmail.com>
2023-08-14 23:54:22 +0800

    feat: refactor the code

---
+++ scripts/blogdata.js
@@ -0,0 +1,20 @@
+var blogData = [
+    {
+        title: "Leading Self Program: My DISC Report",
+        date: "Jun 28, 2023",
+        content: "The DISC profile is a method used to understand human behavior and personality, providing insights into how individuals think, act, and interact with others in specific environments.",
+        link: "blogs/blog2.html"
+    },
+    {
+        title: "Exploring PostgreSQL: My Learning Notebook",
+        date: "Dec 27, 2021",
+        content: "This blog post serves as my personal notes on what I have learned about PostgreSQL. It covers the setup process of PostgreSQL and pgadmin4 and provides a comprehensive list of SQL keywords and examples. Additionally, I compare PostgreSQL and MySQL, and discuss the distinctions between NoSQL and RDBMS.",
+        link: "blogs/blog1.html"
+    },
+    {
+        title: "Google Singapore Office Tour",
+        date: "Jan 11, 2019",
+        content: "Google Singapore Office One-Day Tour.",
+        link: "blogs/blog3.html"
+    },
+];
